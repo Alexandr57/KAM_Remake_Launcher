21bce501fca1ea0bf4a92ad17cefd5447b64728a
Alexandr_7 <borisov.alexandr.7@gmail.com>
2018-05-26 17:11:01 +0500

    Update Code

--- AL7_frmManageCampaigns.Designer.cs
+++ AL7_frmManageCampaigns.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AL7_frmManageCampaigns));
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnAddCampaigns = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.bprgBar = new ProgressBars.Basic.BasicProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(140, 194);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(100, 50);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btnAddCampaigns
+            // 
+            this.btnAddCampaigns.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddCampaigns.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddCampaigns.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAddCampaigns.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAddCampaigns.ForeColor = System.Drawing.Color.Yellow;
+            this.btnAddCampaigns.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAddCampaigns.Location = new System.Drawing.Point(42, 12);
+            this.btnAddCampaigns.Name = "btnAddCampaigns";
+            this.btnAddCampaigns.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnAddCampaigns.Size = new System.Drawing.Size(274, 28);
+            this.btnAddCampaigns.TabIndex = 6;
+            this.btnAddCampaigns.Text = "Добавить кампанию";
+            this.btnAddCampaigns.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(42, 46);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(274, 56);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить кампанию в архив для отправки";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClose.ForeColor = System.Drawing.Color.Yellow;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(42, 346);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnClose.Size = new System.Drawing.Size(274, 28);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // bprgBar
+            // 
+            this.bprgBar.BackColor = System.Drawing.Color.Transparent;
+            this.bprgBar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bprgBar.BackgroundImage")));
+            this.bprgBar.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(21)))), ((int)(((byte)(14)))));
+            this.bprgBar.BorderThickness = 4;
+            this.bprgBar.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bprgBar.ForeColor = System.Drawing.Color.Lime;
+            this.bprgBar.Location = new System.Drawing.Point(42, 312);
+            this.bprgBar.Name = "bprgBar";
+            this.bprgBar.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.bprgBar.Size = new System.Drawing.Size(274, 28);
+            this.bprgBar.TabIndex = 13;
+            this.bprgBar.TextColor = System.Drawing.Color.White;
+            this.bprgBar.Value = 50;
+            // 
+            // AL7_frmManageCampaigns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(360, 397);
+            this.Controls.Add(this.bprgBar);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAddCampaigns);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AL7_frmManageCampaigns";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "AL7_frmManageCampaigns";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnAddCampaigns;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
+        private ProgressBars.Basic.BasicProgressBar bprgBar;
     }
 }
